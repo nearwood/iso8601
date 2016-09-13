9a2ec55caaacc914f7c350943b5b54d70d9de7f6
Nick Earwood <nearwood@gmail.com>
2016-09-13 12:00:33 -0400

    initial files.

---
+++ index.js
@@ -0,0 +1,15 @@
+
+//"2016-09-08T16:03:07.817Z"
+//var reISO = /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2}(?:\.\d*))(?:Z|(\+|-)([\d|:]*))?$/;
+
+//Don't need the capture groups
+var reISO8601 = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(?:\.\d*)(?:Z|(\+|-)(?:[\d|:]*))?$/;
+
+module.exports.parse = function (value) {
+    if (typeof value === 'string') {
+        if (reISO8601.exec(value)) {
+            return new Date(value);
+        }
+    }
+    return value;
+};
