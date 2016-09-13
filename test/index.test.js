302fc4b95fb5a4fb22e839ff869d3e034721454a
Nick Earwood <nearwood@gmail.com>
2016-09-13 12:22:20 -0400

    update tests

--- test/index.test.js
+++ test/index.test.js
@@ -4,18 +4,21 @@ var iso8601 = require('../');
 
 describe('iso8601', function() {
 
+  it('should parse this date', function() {
     var date = "2016-09-08T16:03:07.817Z";
     var expected = new Date(date);
 	
     assert.deepEqual(iso8601(date), expected);
   });
 
+  it('should parse this date too', function() {
     var date = "2016-09-08T16:03:07.817Z";
     var expected = new Date(date);
 	
     assert.deepEqual(iso8601(date), expected);
   });
 
+  it('should not parse this date', function() {
     var date = "2016-tuesday-08T00:00:00.0Z";
     var expected = new Date(date);
 	
